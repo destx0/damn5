47f11cb51ee618ac38e79af96fe07864508f02fc
destx0 <destisdbest@gmail.com>
2024-07-07 14:33:19 +0530

    rr

--- src/renderer/src/components/ImportExportCard.jsx
+++ src/renderer/src/components/ImportExportCard.jsx
@@ -4,7 +4,7 @@ import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 import { Download, Upload, FileInput } from 'lucide-react'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const container = {
   hidden: { opacity: 1, scale: 0.5 },
@@ -15,19 +15,22 @@ const container = {
       delayChildren: 0.1,
       staggerChildren: 0.1
     }
-  }
+  },
+  exit: { opacity: 0, scale: 0.5, transition: { duration: 0.2 } }
 }
 
 const item = {
   hidden: { y: 10, opacity: 0 },
-  visible: { y: 0, opacity: 1 }
+  visible: { y: 0, opacity: 1 },
+  exit: { y: -10, opacity: 0 }
 }
 
 const iconVariants = {
   hover: { scale: 1.2 },
   tap: { scale: 0.8 },
   hidden: { opacity: 0, y: -250 },
-  visible: { opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.3 } }
+  visible: { opacity: 1, y: 0, transition: { ease: 'easeIn', duration: 0.3 } },
+  exit: { opacity: 0, y: 250, transition: { ease: 'easeOut', duration: 0.3 } }
 }
 
 const ImportExportCard = () => {
@@ -125,78 +128,91 @@ const ImportExportCard = () => {
   }
 
   return (
-    <motion.div className="w-[400px]" variants={container} initial="hidden" animate="visible">
-      <Card>
-        <CardHeader>
-          <CardTitle className="flex items-center space-x-2">
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              whileHover="hover"
-              whileTap="tap"
-              variants={iconVariants}
-            >
-              <FileInput className="h-5 w-5" />
+    <AnimatePresence>
+      <motion.div
+        className="w-[400px]"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex items-center space-x-2">
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                whileHover="hover"
+                whileTap="tap"
+                variants={iconVariants}
+              >
+                <FileInput className="h-5 w-5" />
+              </motion.div>
+              <span>Import/Export</span>
+            </CardTitle>
+            <CardDescription>Manage your student data</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-6">
+            <motion.div className="grid gap-2" variants={item}>
+              <Label htmlFor="importButton" className="text-sm font-medium">
+                Import File
+              </Label>
+              <Button
+                id="importButton"
+                onClick={handleImport}
+                className="flex items-center justify-center w-full"
+                variant="outline"
+              >
+                <motion.div
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  whileHover="hover"
+                  whileTap="tap"
+                  variants={iconVariants}
+                >
+                  <Upload className="mr-2 h-4 w-4" />
+                </motion.div>
+                Import CSV/XLSX
+              </Button>
+              <p className="text-xs text-muted-foreground">
+                Import student data from a CSV or XLSX file
+              </p>
             </motion.div>
-            <span>Import/Export</span>
-          </CardTitle>
-          <CardDescription>Manage your student data</CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-6">
-          <motion.div className="grid gap-2" variants={item}>
-            <Label htmlFor="importButton" className="text-sm font-medium">
-              Import File
-            </Label>
-            <Button
-              id="importButton"
-              onClick={handleImport}
-              className="flex items-center justify-center w-full"
-              variant="outline"
-            >
-              <motion.div
-                initial="hidden"
-                animate="visible"
-                whileHover="hover"
-                whileTap="tap"
-                variants={iconVariants}
-              >
-                <Upload className="mr-2 h-4 w-4" />
-              </motion.div>
-              Import CSV/XLSX
-            </Button>
-            <p className="text-xs text-muted-foreground">
-              Import student data from a CSV or XLSX file
-            </p>
-          </motion.div>
 
-          <Separator />
+            <Separator />
 
-          <motion.div className="grid gap-2" variants={item}>
-            <Label htmlFor="exportButton" className="text-sm font-medium">
-              Export Data
-            </Label>
-            <Button
-              id="exportButton"
-              onClick={handleExport}
-              className="flex items-center justify-center w-full"
-              variant="outline"
-            >
-              <motion.div
-                initial="hidden"
-                animate="visible"
-                whileHover="hover"
-                whileTap="tap"
-                variants={iconVariants}
+            <motion.div className="grid gap-2" variants={item}>
+              <Label htmlFor="exportButton" className="text-sm font-medium">
+                Export Data
+              </Label>
+              <Button
+                id="exportButton"
+                onClick={handleExport}
+                className="flex items-center justify-center w-full"
+                variant="outline"
               >
-                <Download className="mr-2 h-4 w-4" />
-              </motion.div>
-              Export to XLSX
-            </Button>
-            <p className="text-xs text-muted-foreground">Export all student data to an XLSX file</p>
-          </motion.div>
-        </CardContent>
-      </Card>
-    </motion.div>
+                <motion.div
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  whileHover="hover"
+                  whileTap="tap"
+                  variants={iconVariants}
+                >
+                  <Download className="mr-2 h-4 w-4" />
+                </motion.div>
+                Export to XLSX
+              </Button>
+              <p className="text-xs text-muted-foreground">
+                Export all student data to an XLSX file
+              </p>
+            </motion.div>
+          </CardContent>
+        </Card>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
