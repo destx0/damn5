90973ca9b5aa1fecbd3e88a45b1846d868b81c06
destx0 <destisdbest@gmail.com>
2024-07-19 12:21:06 +0530

    good

--- src/renderer/src/components/generateCertificate.jsx
+++ src/renderer/src/components/generateCertificate.jsx
@@ -3,61 +3,7 @@ import html2canvas from 'html2canvas'
 import jsPDF from 'jspdf'
 import { toast } from 'sonner'
 import { format, parse } from 'date-fns'
-
-const numberToWords = (num) => {
-  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-  const tens = [
-    '',
-    '',
-    'twenty',
-    'thirty',
-    'forty',
-    'fifty',
-    'sixty',
-    'seventy',
-    'eighty',
-    'ninety'
-  ]
-  const teens = [
-    'ten',
-    'eleven',
-    'twelve',
-    'thirteen',
-    'fourteen',
-    'fifteen',
-    'sixteen',
-    'seventeen',
-    'eighteen',
-    'nineteen'
-  ]
-
-  if (num < 10) return ones[num]
-  if (num < 20) return teens[num - 10]
-  if (num < 100) return tens[Math.floor(num / 10)] + (num % 10 !== 0 ? '-' + ones[num % 10] : '')
-  if (num < 1000)
-    return (
-      ones[Math.floor(num / 100)] +
-      ' hundred' +
-      (num % 100 !== 0 ? ' and ' + numberToWords(num % 100) : '')
-    )
-  return (
-    numberToWords(Math.floor(num / 1000)) +
-    ' thousand' +
-    (num % 1000 !== 0 ? ' ' + numberToWords(num % 1000) : '')
-  )
-}
-
-const dateToWords = (dateString) => {
-  const date = parse(dateString, 'yyyy-MM-dd', new Date())
-  const day = format(date, 'd')
-  const month = format(date, 'MMMM')
-  const year = format(date, 'yyyy')
-
-  const dayInWords = numberToWords(parseInt(day))
-  const yearInWords = numberToWords(parseInt(year))
-
-  return `${dayInWords} ${month} ${yearInWords}`
-}
+import dateConverter from '@nexisltd/date2word'
 
 const generateCertificate = async (student) => {
   try {
@@ -80,10 +26,13 @@ const generateCertificate = async (student) => {
       return `<strong>${label}:</strong> <span style="display: inline-block; position: relative; width: ${size}ch;">${field}${padding}<span style="position: absolute; bottom: -1px; left: 0; right: 0; border-bottom: 1px solid black;"></span></span>`
     }
 
-    const dateOfBirthInWords = dateToWords(updatedStudent.dateOfBirth)
+    const dateOfBirthInWords = dateConverter(new Date(updatedStudent.dateOfBirth), {
+      isCapitalized: true,
+      isDateFirst: true
+    })
 
     const certificateHTML = `
-      <div id="certificate" style="width: 210mm; height: 297mm; padding: 20mm 12mm; box-sizing: border-box; background-color: white; font-family: 'Times New Roman', Times, serif; border: 2px solid black; font-size: 13pt; position: relative;">
+      <div id="certificate" style="width: 210mm; height: 297mm; padding: 20mm 12mm; box-sizing: border-box; background-color: white; font-family: 'Times New Roman', Times, serif; border: 2px solid black; font-size: 12pt; position: relative;">
         <style>
           #certificate pre {
             line-height: 2;
@@ -93,9 +42,9 @@ const generateCertificate = async (student) => {
           }
         </style>
         <div style="position: absolute; top: 10mm; left: 10mm; right: 10mm; bottom: 10mm; border: 2px solid black;"></div>
-<h3 style="text-align: center; font-weight: bold; margin-top: -40px; margin-bottom: -6x;">Jaggannath Shikshan Prasarak Mandal's</h3>
-<h2 style="text-align: center; font-size: 23px; font-weight: bold;  margin-bottom: -6px;">Shashikant Sakharam Chaudhari Kanya Vidyalay, Yawal</h2>
-<h3 style="text-align: center; font-weight: bold; margin-top: 0;">Taluka- Yawal, Dist. Jalgaon</h3>
+        <h3 style="text-align: center; font-weight: bold; margin-top: -40px; margin-bottom: -6x;">Jaggannath Shikshan Prasarak Mandal's</h3>
+        <h2 style="text-align: center; font-size: 23px; font-weight: bold;  margin-bottom: -6px;">Shashikant Sakharam Chaudhari Kanya Vidyalay, Yawal</h2>
+        <h3 style="text-align: center; font-weight: bold; margin-top: 0;">Taluka- Yawal, Dist. Jalgaon</h3>
         <p style="text-align: center; margin-top: -6px;">Phone No. 02585-261290 &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; E Mail - mksyawal@yahoo.in</p>
         <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
           <span><strong>Sr. No.</strong> ${createField('', serialNumber.toString(), 10)}</span>
@@ -104,20 +53,20 @@ const generateCertificate = async (student) => {
         <div style="display: flex; justify-content: flex-end; margin-bottom: 10px;">
           <span><strong>TEN:</strong> ${createField('', updatedStudent.ten, 30)}</span>
         </div>
-<div style="margin-bottom: 10px;">
-  <strong>School Reg. No.</strong>- Edu. Depu.Dir/Sec-2/First Appru/
-  <span style="float: right;"><strong>Medium:</strong> Marathi</span>
-  <br>
-  <span style="padding-left: 7.5em;">90-91/92/Div.Sec.Depu.Dir.Nashik/Datted 12-3-92</span>
-</div>
+        <div style="margin-bottom: 10px;">
+          <strong>School Reg. No.</strong>- Edu. Depu.Dir/Sec-2/First Appru/
+          <span style="float: right;"><strong>Medium:</strong> Marathi</span>
+          <br>
+          <span style="padding-left: 7.5em;">90-91/92/Div.Sec.Depu.Dir.Nashik/Datted 12-3-92</span>
+        </div>
         <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
           <span><strong>U Dise No.</strong>- 27031508414</span>
           <span><strong>Board</strong>- Nashik</span>
           <span><strong>Index No.</strong>- 15.15.005</span>
         </div>
         <div style="text-align: center; margin-bottom: 20px;">
-  <span style="font-size: 36px; font-weight: bold; text-decoration: underline;">Leaving Certificate</span>
-</div>
+          <span style="font-size: 36px; font-weight: bold; text-decoration: underline;">Leaving Certificate</span>
+        </div>
         <pre style="line-height: 1.6; padding: 10px 0; font-family: inherit; font-size: inherit; margin: 0; white-space: pre-wrap; word-wrap: break-word;">
 ${createField('Student ID', updatedStudent.studentId, 25)} ${createField('U.I.D. No. (Aadhar Card No.)', updatedStudent.aadharNo, 15)}
 <strong>Name of the student in full</strong>
